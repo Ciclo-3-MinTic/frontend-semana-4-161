c45c9424a1606f3759f01a9fda56c57d6f7789bd
imcrisam <imcrisam@gmail.com>
2020-12-18 16:02:58 -0500

    usasr in home

--- src/views/HomeAdmin.vue
+++ src/views/HomeAdmin.vue
@@ -6,14 +6,95 @@
         <v-navigation-drawer
           v-model="primaryDrawer.model"
           :clipped="true"
-                   
           app
           overflow
-        ></v-navigation-drawer>
+        >
+          <v-list dense>
+            <template>
+              <v-card >
+                <v-card-text class="pb-0">
+                  <v-row align="center" justify="space-around">
+                    <v-col align-self="start" class="pa-0 " cols="2">
+                      <v-avatar color="orange ml-3" size="62">
+                        <span class="white--text headline">A</span>
+                      </v-avatar>
+                    </v-col>
+                    <v-col class="py-0" cols="10">
+                      <v-list-item >
+                        <v-list-item-content class="text-right">
+                          <v-list-item-title class="title">
+                            rol
+                          </v-list-item-title>
+                          <v-list-item-subtitle
+                            >Name</v-list-item-subtitle
+                          >
+                          <v-list-item-subtitle
+                            >Email</v-list-item-subtitle
+                          >
+                          
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-col>
+                  </v-row>
+
+                  <v-row align="center" justify="space-around">
+                    <v-col cols="6">
+                      <v-btn tile color="error" small>
+                        <v-icon left> mdi-keyboard-backspace </v-icon>
+                        Salir
+                      </v-btn>
+                    </v-col>
+                    <v-col cols="6">
+                      <v-btn-toggle v-model="$vuetify.theme.dark" rounded small>
+                        <v-btn small>
+                          <v-icon>mdi-brightness-5</v-icon>
+                        </v-btn>
+
+                        <v-btn small>
+                          <v-icon>mdi-brightness-4</v-icon>
+                        </v-btn>
+                      </v-btn-toggle>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+              </v-card>
+              <v-list-item @click="home">
+                <v-list-item-action>
+                  <v-icon>home</v-icon>
+                </v-list-item-action>
+                <v-list-item-title> Inicio </v-list-item-title>
+              </v-list-item>
+            </template>
+            <template>
+              <v-list-group>
+                <v-list-item slot="activator">
+                  <v-list-item-content>
+                    <v-list-item-title> Almacén </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item :to="{ name: 'categoria' }">
+                  <v-list-item-action>
+                    <v-icon>table_chart</v-icon>
+                  </v-list-item-action>
+                  <v-list-item-content>
+                    <v-list-item-title> Categorías </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item :to="{ name: 'articulo' }">
+                  <v-list-item-action>
+                    <v-icon>table_chart</v-icon>
+                  </v-list-item-action>
+                  <v-list-item-content>
+                    <v-list-item-title> Artículos </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-group>
+            </template>
+          </v-list>
+        </v-navigation-drawer>
 
         <v-app-bar :clipped-left="true" app>
           <v-app-bar-nav-icon
-            
             @click.stop="primaryDrawer.model = !primaryDrawer.model"
           ></v-app-bar-nav-icon>
           <v-toolbar-title>Vuetify</v-toolbar-title>
@@ -22,32 +103,7 @@
         <v-main>
           <v-container fluid>
             <v-row align="center" justify="center">
-              <v-col cols="10">
-                <v-card>
-                  <v-card-text>
-                    <v-row>
-                      <v-col cols="12" md="6">
-                        <span>Scheme</span>
-                        <v-switch
-                          v-model="$vuetify.theme.dark"
-                          primary
-                          label="Dark"
-                        ></v-switch>
-                      </v-col>
-                      <v-col cols="12" md="6">
-                        
-                        
-                        
-                      </v-col>
-                      <v-col cols="12" md="6">
-                        <span>Footer</span>
-                       
-                      </v-col>
-                    </v-row>
-                  </v-card-text>
-                  
-                </v-card>
-              </v-col>
+              <v-col cols="10"> </v-col>
             </v-row>
           </v-container>
         </v-main>
@@ -70,10 +126,32 @@ export default {
       primaryDrawer: {
         model: null,
         type: "default (no property)",
-        
-        
       },
     };
+  },
+
+  computed: {
+    logueado() {
+      /* return this.$store.state.usuario; */
+      return null;
+    },
+  },
+  methods: {
+    salir() {
+      /* this.$store.dispatch("salir"); */
+      this.estado = 1;
+    },
+
+    ingresar() {
+      this.estado = 0;
+    },
+
+    home() {
+      this.estado = 1;
+    },
+  },
+  created() {
+    /* this.$store.dispatch("autoLogin"); */
   },
 };
 </script>
