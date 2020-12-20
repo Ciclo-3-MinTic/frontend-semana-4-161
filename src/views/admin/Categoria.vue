abeffb943c6a456d7007428692d5da730ac0237d
imcrisam <imcrisam@gmail.com>
2020-12-20 17:58:50 -0500

    crud categorias

--- src/views/admin/Categoria.vue
+++ src/views/admin/Categoria.vue
@@ -1,282 +1,274 @@
 <template>
-    <v-layout align-start>
-        <v-flex>
-            <v-toolbar text color="white">
-                <v-toolbar-title>Categorías</v-toolbar-title>
-                <v-divider
-                class="mx-2"
-                inset
-                vertical
-                ></v-divider>
-                <v-spacer></v-spacer>
-                <v-text-field class="text-xs-center" v-model="search" append-icon="mdi-card-search" 
-                label="Búsqueda" single-line hide-details></v-text-field>
-                <v-spacer></v-spacer>
-                <v-dialog v-model="dialog" max-width="500px">
-                    <template v-slot:activator="{ on }">
-                        <v-btn color="primary" dark class="mb-2" v-on="on">Nuevo</v-btn>
-                    </template>
-                    <v-card>
-                        <v-card-title>
-                        <span class="headline">{{ formTitle }}</span>
-                        </v-card-title>            
-                        <v-card-text>
-                        <v-container grid-list-md>
-                            <v-layout wrap>
-                                <v-flex xs12 sm12 md12>
-                                    <v-text-field v-model="nombre" label="Nombre"></v-text-field>
-                                </v-flex>
-                                <v-flex xs12 sm12 md12>
-                                    <v-text-field v-model="descripcion" label="Descripción"></v-text-field>
-                                </v-flex>
-                                <v-flex xs12 sm12 md12 v-show="valida">
-                                    <div class="red--text" v-for="v in validaMensaje" :key="v" v-text="v">
-
-                                    </div>
-                                </v-flex>
-                            </v-layout>
-                        </v-container>
-                        </v-card-text>            
-                        <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1"  @click="close">Cancelar</v-btn>
-                        <v-btn color="blue darken-1"  @click="guardar">Guardar</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
-                <v-dialog v-model="adModal" max-width="290">
-                    <v-card>
-                        <v-card-title class="headline" v-if="adAccion==1">
-                            Activar Item
-                        </v-card-title>
-                        <v-card-title class="headline" v-if="adAccion==2">
-                            Desactivar Item
-                        </v-card-title>
-                        <v-card-text>
-                            Estás a punto de <span v-if="adAccion==1">activar </span>
-                            <span v-if="adAccion==2">desactivar </span> el item {{adNombre}}
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn @click="activarDesactivarCerrar()" color="green darken-1" >
-                                Cancelar
-                            </v-btn>
-                            <v-btn v-if="adAccion==1" @click="activar()" color="orange darken-4" >
-                                Activar
-                            </v-btn>
-                            <v-btn v-if="adAccion==2" @click="desactivar()" color="orange darken-4" >
-                                Desactivar
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
-            </v-toolbar>
-            <v-data-table
-                :headers="headers"
-                :items="categorias"
-                :search="search"
-                class="elevation-1"
-            >
-            <template v-slot:[`item.opciones`]="{ item }">
-                <v-icon
-                    small
-                    class="mr-2"
-                    @click="editItem(item)"
-                    >
-                    edit
-                    </v-icon>
-                        <v-icon v-if="item.estado"
-                        small
-                        @click="activarDesactivarMostrar(2,item)"
-                        >
-                        block
-                        </v-icon>
-                    
-                        <v-icon v-else
-                        small
-                        @click="activarDesactivarMostrar(1,item)"
-                        >
-                        check
-                        </v-icon>
-            </template>
-            <template v-slot:[`item.estado`]="{ item }">
-                 <div v-if="item.estado">
-                        <span class="blue--text">Activo</span>
-                    </div>
-                    <div v-else>
-                        <span class="red--text">Inactivo</span>
-                    </div>
-            </template>
-                <template v-slot:no-data>
-                <v-btn color="primary" @click="listar()">Resetear</v-btn>
-                </template>
-            </v-data-table>
-        </v-flex>
-    </v-layout>
-</template>
-<script>
-    import axios from 'axios'
-    export default {
-        data(){
-            return{
-                dialog: false,
-                search:'',
-                categorias:[],
-                headers: [
-                    { text: 'Opciones', value: 'opciones', sortable: false },
-                    { text: 'Nombre', value: 'nombre', sortable: true },
-                    { text: 'Descripción', value: 'descripcion', sortable: false },
-                    { text: 'Estado', value: 'estado', sortable: false },
-                ],
-                editedIndex: -1,
-                id:'',
-                nombre:'',
-                descripcion:'',
-                valida:0,
-                validaMensaje:[],
-                adModal:0,
-                adAccion:0,
-                adNombre:'',
-                adId:''
-            }
-        },
-        computed: {
-            formTitle () {
-            return this.editedIndex === -1 ? 'Nuevo registro' : 'Editar registro'
-            }
-        },
-        watch: {
-            dialog (val) {
-            val || this.close()
-            }
-        },
-        created () {
-            this.listar()
-        },
-        methods: {
-            listar(){
-                let me=this;
-                let header={"Token" : this.$store.state.token};
-                let configuracion= {headers : header};            
-                axios.get('categoria/list',configuracion).then(function (response){
-                    me.categorias=response.data;
-                }).catch(function(error){
-                    console.log(error);
-                });
-            },
-            limpiar(){
-                this.id='';
-                this.nombre='';
-                this.descripcion='';
-                this.valida=0;
-                this.validaMensaje=[];
-                this.editedIndex=-1;
-            },
-            validar(){
-                this.valida=0;
-                this.validaMensaje=[];
-                if(this.nombre.length<1 || this.nombre.length>50){
-                    this.validaMensaje.push('El nombre de la categoría debe tener entre 1-50 caracteres.');
-                }
-                if(this.descripcion.length>255){
-                    this.validaMensaje.push('La descripción de la categoría no debe tener más de 255 caracteres.');
-                }
-                if (this.validaMensaje.length){
-                    this.valida=1;
-                }
-                return this.valida;
-            },
-            guardar(){
-                let me=this;
-                let header={"Token" : this.$store.state.token};
-                let configuracion= {headers : header};
-                if (this.validar()){
-                    return;
-                }
-                if (this.editedIndex >-1){
-                    //Código para editar
-                    axios.put('categoria/update',{'id':this.id,'nombre':this.nombre,'descripcion':this.descripcion},configuracion)
-                    .then(function(response){
-                        me.limpiar();
-                        me.close();
-                        me.listar();
-                    })
-                    .catch(function(error){
-                        console.log(error);
-                    });
-                }else{
-                    //Código para guardar
-                    axios.post('categoria/add',{'nombre':this.nombre,'descripcion':this.descripcion},configuracion)
-                    .then(function(response){
-                        me.limpiar();
-                        me.close();
-                        me.listar();
-                    })
-                    .catch(function(error){
-                        console.log(error);
-                    });
-                }
-            },
-            editItem (item) {
-                this.id=item.id;
-                this.nombre=item.nombre;
-                this.descripcion=item.descripcion;
-                this.dialog = true;
-                this.editedIndex=1;
-            },
-            activarDesactivarMostrar(accion,item){
-                this.adModal=1;
-                this.adNombre=item.nombre;
-                this.adId=item.id;
-                if (accion==1){
-                    this.adAccion=1;
-                } else if(accion==2){
-                    this.adAccion=2;
-                } else{
-                    this.adModal=0;
-                }
-            },
-            activarDesactivarCerrar(){
-                this.adModal=0;
-            },
-            activar(){
-                let me=this;
-                let header={"Token" : this.$store.state.token};
-                let configuracion= {headers : header};
+  <v-layout align-start d-block flex-column block>
+    <v-dialog v-model="dialog">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ title }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+               <v-flex xs8 sm9 md10>
+                <v-text-field v-model="nombre" label="Nombre"> </v-text-field>
+              </v-flex>
+                <v-flex xs4 sm3 md2>
+                <v-text-field v-model="id" label="codigo" disabled v-show="typeDialog" type='text' > </v-text-field>
+              </v-flex>
+           
             
-                axios.put('categoria/activate',{'id':this.adId},configuracion)
-                    .then(function(response){
-                        me.adModal=0;
-                        me.adAccion=0;
-                        me.adNombre='';
-                        me.adId='';
-                        me.listar();
-                    })
-                    .catch(function(error){
-                        console.log(error);
-                    });
-            },
-            desactivar(){
-                let me=this;
-                let header={"Token" : this.$store.state.token};
-                let configuracion= {headers : header};
-                axios.put('categoria/deactivate',{'id':this.adId},configuracion)
-                    .then(function(response){
-                        me.adModal=0;
-                        me.adAccion=0;
-                        me.adNombre='';
-                        me.adId='';
-                        me.listar();
-                    })
-                    .catch(function(error){
-                        console.log(error);
-                    });
-            },
-            close () {
-                this.dialog = false;
-            },
+              <v-flex xs12>
+                <v-textarea  v-model="descripcion" outlined rows="3" auto-grow label="descripcion">
+                </v-textarea>
+              </v-flex>
              
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" @click="dialog = false">Cancelar</v-btn>
+          <v-btn color="blue darken-1" @click="dialogAcepter">Guardar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
+    <data-table-base
+      :title="titles"
+      :data="dataCategorias"
+      :headers="headers"
+      :isloading="isloading"
+      @reroll="reroll"
+      @add="add"
+      @edit="edit"
+      @desactive="desactive"
+      @active="active"
+    >
+    </data-table-base>
+  </v-layout>
+</template>
+
+<script>
+import axios from "axios";
+import DataTableBase from "@/components/DataTableBase.vue";
+export default {
+  components: {
+    DataTableBase,
+  },
+  data() {
+    return {
+      dialog: false,
+      dialogpass: false,
+      dialogAlert: false,
+      typeDialog: 0, //add=0, edit=1;
+      isloading: true,
+      title: "Usuario",
+      titles: "Usuarios",
+      dataCategorias: [],
+      roles: [],
+      tipo_documentos: [],
+      headers: [
+        { text: "Código", value: "id", sortable: true, align: "center" },
+        { text: "Nombre", value: "nombre", sortable: true, align: "center" },
+        {
+          text: "Descripcion",
+          value: "descripcion",
+          sortable: false,
+          align: "center",
+        },
+        {
+          text: "Estado",
+          value: "estado",
+          sortable: true,
+          align: "center",
+        },
+        {
+          text: "Opciones",
+          value: "opciones",
+          sortable: false,
+          align: "center",
+        },
+      ],
+
+      valida: 0,
+      id: "",
+      nombre: "",
+      descripcion: "",
+      estado: 0
+    };
+  },
+
+  created() {
+    this.listar();
+  },
+  methods: {
+    editItem(item) {
+      (this.id = item.id),
+        (this.nombre = item.nombre),
+        (this.descripcion = item.descripcion),
+        (this.estado = item.estado)
+       
+    },
+    clearItem() {
+       (this.id = ""),
+        (this.nombre = ""),
+        (this.descripcion = ""),
+        (this.estado = "")
+    },
+    ///----inicio metodos de data table base
+    reroll() {
+      this.isloading = true;
+
+      this.listar();
+    },
+    add() {
+      this.typeDialog = 0;
+      this.clearItem();
+      this.openDialog();
+    },
+    edit(item) {
+      this.typeDialog = 1;
+      this.editItem(item);
+      this.openDialog();
+    },
+    desactive(item) {
+      this.isloading = true;
+      this.deactivateCategorias(item);
+    },
+    active(item) {
+      this.isloading = true;
+      this.activateCategorias(item);
+    },
+    //----fin metodos data table base
+
+    //-- inicio dialogos
+    openDialog() {
+      this.dialog = true;
+    },
+    closeDialog() {
+      this.clearItem();
+      this.dialog = false;
+      this.typeDialog = 0;
+    },
+
    
+    openDialogResponse(type, mensaje) {
+      this.reroll();
+
+      this.closeDialog();
+      this.isloading = false;
+      console.log(mensaje);
+    },
+    //-- fin dialogos
+
+    //---inicio validaciones
+    validar() {
+      this.valida = 0;
+
+      
+      if (this.nombre.length < 1 || this.nombre.length > 50) {
+        // nombre muy corto o muy largo
+      }
+     
+      if (this.descripcion < 1) {
+        // telefono muy corto
+      }
+      return this.valida;
+    },
+   
+    //-- fin validaciones
+
+    dialogAcepter() {
+      if (this.validar) {
+        if (this.typeDialog) {
+          this.updateCategorias({
+            id: this.id,
+            nombre: this.nombre,
+            descripcion: this.descripcion
+
+          })
+        } else {
+          this.newCategorias({
+            
+            nombre: this.nombre,
+            descripcion: this.descripcion
+          })
         }
-    }
+      }
+    },
+  
+    //--- accions hacia la api--
+    headerToken() {
+      let header = { Token: this.$store.state.token };
+      return { headers: header };
+    },
+
+    listar() {
+      let me = this;
+      axios
+        .get("categoria/list", this.headerToken())
+        .then(function (response) {
+          me.dataCategorias = response.data;
+          me.isloading = false;
+        })
+        .catch(function (error) {
+          me.isloading = false;
+          console.log(error);
+        });
+    },
+
+    newCategorias(usuario) {
+      let me = this;
+      axios
+        .post("categoria/add", usuario, this.headerToken())
+        .then(function (response) {
+          me.openDialogResponse(1, "nuevo usuario creado");
+        })
+        .catch(function (error) {
+          me.openDialogResponse(0, "no se puede crear usuaro");
+          console.log(error);
+        });
+    },
+    updateCategorias(usuario) {
+      let me = this;
+      axios
+        .put("categoria/update", usuario, this.headerToken())
+        .then(function (response) {
+          me.openDialogResponse(1, "usuario actualizado");
+        })
+        .catch(function (error) {
+          me.openDialogResponse(0, "no se puede actualizar usuaro");
+          console.log(error);
+        });
+    },
+
+    activateCategorias(usuario) {
+      let me = this;
+      axios
+        .put("categoria/activate", { id: usuario.id }, this.headerToken())
+        .then(function (response) {
+          me.openDialogResponse(1, "usuario activado");
+        })
+        .catch(function (error) {
+          me.openDialogResponse(0, "no se puede activar usuario");
+        });
+    },
+    deactivateCategorias(usuario) {
+      let me = this;
+      axios
+        .put("categoria/deactivate", { id: usuario.id }, this.headerToken())
+        .then(function (response) {
+          me.openDialogResponse(1, "usuario activado");
+        })
+        .catch(function (error) {
+          me.openDialogResponse(0, "no se puede activar usuario");
+        });
+    },
+
+    //--- fin accions hacia la api--
+  },
+};
 </script>
+
+<style>
+</style>
