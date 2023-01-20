29ca58a71059cb3a03be2d9087a2196bb7bbe330
Imcrisam <Imcrisam@gmail.com>
2023-01-20 15:51:22 -0500

    add new apirest

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 import store from "./store";
-import vuetify from './plugins/vuetify';
+import vuetify from "./plugins/vuetify";
 import axios from "axios";
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 /* axios.defaults.baseURL = 'http://localhost:3000/api/'; */
-axios.defaults.baseURL = 'https://shrouded-forest-70309.herokuapp.com/api/';
+axios.defaults.baseURL =
+  "https://backend-seamana-4-161-production.up.railway.app/api/";
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
