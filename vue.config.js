caef5ca7c72f73d40163a5b1f83f9d058327c8ea
Imcrisam <Imcrisam@gmail.com>
2023-01-19 21:05:28 -0500

    public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/frontend-semana-4-161/'
+    : '/',
   "transpileDependencies": [
     "vuetify"
   ]
